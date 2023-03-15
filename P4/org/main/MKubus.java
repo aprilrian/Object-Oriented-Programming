8667def7086916bfdad7d9653efdf29ca2e35dd9
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2023-03-15 19:15:10 +0700

    Add files via upload

--- P4/org/main/MKubus.java
+++ P4/org/main/MKubus.java
@@ -12,7 +12,7 @@ public class MKubus {
     public static void main(String[] args) {
         BujurSangkar bujurSangkar = new BujurSangkar(4);
         Kubus kubus = new Kubus(bujurSangkar);
-        System.out.println("Volume Kubus: "+kubus.hitungVolume());
-        System.out.println("Luas Kubus: "+kubus.hitungLuasAlas());
+        System.out.println("Luas permukaan kubus dengan panjang sisi 4 satuan: "+kubus.hitungLuasAlas());
+        System.out.println("Volume kubus dengan panjang sisi 4 satuan: "+kubus.hitungVolume());
     }
 }
